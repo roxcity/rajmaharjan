134170392eb1a9d8267e7de4790620c51df8234a
Raj Krishna Maharjan <raj.maharjan33@gmail.com>
2020-02-14 15:25:26 +0545

    Updates

---
+++ precache-manifest.78c63ea9b69bf9c812e168146ff699b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e9bd5bb82e22db39a0d78ceb9182fbe",
+    "url": "/roxcity.github.io/index.html"
+  },
+  {
+    "revision": "9a812bdfcc8bf284e587",
+    "url": "/roxcity.github.io/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c9bbac0bff73ca2d67e2",
+    "url": "/roxcity.github.io/static/js/2.4723e2f6.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/roxcity.github.io/static/js/2.4723e2f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a812bdfcc8bf284e587",
+    "url": "/roxcity.github.io/static/js/main.793a4d72.chunk.js"
+  },
+  {
+    "revision": "56ab643e086aaa7f8868",
+    "url": "/roxcity.github.io/static/js/runtime-main.36e5e3ac.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/roxcity.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
